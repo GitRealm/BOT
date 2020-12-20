60a4b85d13ac98095350966fccd2aff9b1f68594
GitRealm <medfordm16@gmail.com>
2020-12-20 14:31:45 -0600

    Renamed files to begin with lowercase Added deploy.lua Added documentation for bore.lua

---
+++ deploy.lua
@@ -0,0 +1,21 @@
+--
+-- deploy.lua
+-- Automated deployment of turtles
+--
+---------------------------------------------------------------
+--
+--
+--
+---------------------------------------------------------------
+--
+-- Functions
+--
+
+local function calculate(chunks)
+	local numTurtles = chunks/4
+	return numTurtles
+end
+
+local function savePos()
+	local x,y,z = gps.locate()
+end

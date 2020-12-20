21cf0babc8760dcf6face0885ff90ae1cef1a958
GitRealm <medfordm16@gmail.com>
2020-12-20 15:13:40 -0600

    Added orient() function for determing which direction the turtle is facing

--- deploy.lua
+++ deploy.lua
@@ -21,9 +21,39 @@ local function savePos()
 	print(x,y,z)
 end
 
+local function navigate(x,y,z,direction)
+
+end
+
+local function orient()
+	local x,y,z = gps.locate()
+	if turtle.detect() then turtle.dig() end
+	turtle.forward()
+	local x2,y2,z2 = gps.locate()
+
+	if x2-x ~= 0 then
+		if x2-x <0 then direction = "posX"
+		else direction = "negX"
+		end
+
+	elseif y2-y ~= 0 then
+		if y2-y <0 then direction = "posY"
+		else direction = "negY"
+		end
+
+	elseif z2-z ~= 0 then
+		if z2-z <0 then direction = "posZ"
+		else direction = "negZ"
+		end
+	end
+
+	print(direction)
+
+end
 ---------------------------------------------------------------
 --
 -- Program Execution
 --
 
 savePos()
+orient()
